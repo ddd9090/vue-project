109c1dd08629da4a865c1ce704faabd0e64d7217
ddd9090 <wsu4717@gmail.com>
2024-12-05 21:54:31 +0800

    人气推荐模块与图片懒加载

--- src/main.js
+++ src/main.js
@@ -7,10 +7,10 @@ import App from './App.vue'
 import router from './router'
 
 import '@/styles/common.scss'
+import {lazyPlugin} from '@/directives'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin)
 app.mount('#app')
-
