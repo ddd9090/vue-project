ff8de7dc617efaee189b204fa46dc6daba9cdab9
ddd9090 <wsu4717@gmail.com>
2024-12-05 21:04:07 +0800

    面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,7 +3,8 @@ import HomeCategory from './components/HomeCategory.vue'
 import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+import homeProduct from './components/homeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -11,6 +12,11 @@ import homeProduct from './components/HomeProduct.vue'
     <HomeCategory />
     <HomeBanner />
   </div>
+  <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+    <div>
+      我是新鲜好物
+    </div>
+  </HomePanel>
   <HomeNew />
   <HomeHot />
   <homeProduct />
