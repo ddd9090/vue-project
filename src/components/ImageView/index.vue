16d7a5dcd3f29ffc3fac7eba4b7cdff18c8153b0
ddd9090 <wsu4717@gmail.com>
2024-12-06 21:54:01 +0800

    放大镜-滑块跟随鼠标移动

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 // 图片列表
-import{ref} from 'vue'
+import{ref, watch} from 'vue'
+import { useMouseInElement } from '@vueuse/core'
 const imageList = [
   "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
   "https://yanxuan-item.nosdn.127.net/e801b9572f0b0c02a52952b01adab967.jpg",
@@ -8,11 +9,30 @@ const imageList = [
   "https://yanxuan-item.nosdn.127.net/f93243224dc37674dfca5874fe089c60.jpg",
   "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg"
 ]
-
+//小图切大图
 const activeIndex = ref(0)
 const enterhandler = (i)=>{
   activeIndex.value = i
 }
+
+//获取鼠标
+const left = ref(0)
+const top = ref(0)
+const target = ref(null)
+const {elementX,elementY,isOutside} = useMouseInElement(target)
+
+watch([elementX,elementY],()=>{
+  if(elementX.value>100&&elementX.value<300){
+    left.value = elementX.value -100
+  }
+  if(elementY.value>100&&elementY.value<300){
+    top.value = elementY.value -100
+  }
+  if(elementX.value>300){left.value=200}
+  if(elementX.value<100){left.value=0}
+  if(elementY.value>300){top.value=200}
+  if(elementY.value<100){top.value=0}
+})
 </script>
 
 
@@ -22,7 +42,7 @@ const enterhandler = (i)=>{
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
