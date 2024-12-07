4482a96d59b1fcd2836b51faf279b73127733876
ddd9090 <wsu4717@gmail.com>
2024-12-07 23:26:50 +0800

    登录-使用token适配多模板

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 // import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
-
 import '@/styles/common.scss'
 import {lazyPlugin} from '@/directives'
 import{componentPlugin}from '@/components'
