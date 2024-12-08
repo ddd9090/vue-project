0f7e9c86ea821468e29ee39951f912241d3967c2
ddd9090 <wsu4717@gmail.com>
2024-12-08 17:14:02 +0800

    会员中心-路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,10 @@ import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import PayBack from '@/views/Pay/PayBack.vue'
+import Member from '@/views/Member/index.vue'
+import MemberInfo from '@/views/Member/components/UserInfo.vue'
+import MemberOrder from '@/views/Member/components/UserOrder.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -47,6 +51,20 @@ const router = createRouter({
         {
           path: 'paycallback', // 注意路径，必须是paycallback
           component: PayBack
+        },
+        {
+          path: '/member',
+          component: Member,
+          children: [
+            {
+              path: '',
+              component: MemberInfo
+            },
+            {
+              path: 'order',
+              component: MemberOrder
+            }
+          ]
         }
       ]
     },
